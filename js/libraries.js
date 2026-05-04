786b3e9a09c36eed244aaffbec5d3e30ade9b4bf
Vaibhavi-06 <fulsundarvaibhavi1@gmail.com>
2026-05-04 13:41:55 +0530

    es6 module

---
+++ js/libraries.js
@@ -0,0 +1,20 @@
+export let message = "Es6 Module";
+
+export function user(name){
+    console.log(`Hello my name ${name}`);
+}
+
+export class test{
+    constructor(){
+        console.log("This is test class constructor");
+    }
+}
+
+//export {message,user,test};
+
+ export let course = "Java";
+
+ export function student(name){
+    console.log(`Student name is ${name}`);
+}
+
---
+++ js/main.js
@@ -0,0 +1,12 @@
+import {message,user,test} from "./libraries.js";
+//import {user} from "./libraries.js";
+
+console.log(message);
+user("Vaibhavi");
+
+let a = new test();
+
+import {course,student} from "./libraries.js";
+
+console.log(course);
+student("Sayli");
